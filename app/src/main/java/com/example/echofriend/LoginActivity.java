60912d8117eed61d8857af4709b5a38bd29c9baf
Busisiwe Mnisi <busisiwemnisi753@gmail.com>
2025-05-13 23:38:51 +0200

    added code for login, I also added the rest of the pages needed for the app subject to change

--- app/src/main/java/com/example/echofriend/LoginActivity.java
+++ app/src/main/java/com/example/echofriend/LoginActivity.java
@@ -1,6 +1,47 @@
 package com.example.echofriend;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends Activity {
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    public EditText username;
+    public EditText password;
+    public Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.loginButton);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = username.getText().toString().trim();
+                String passWord = password.getText().toString().trim();
+
+                if (userName.isEmpty() || passWord.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                } else if (passWord.equals("true") && userName.equals("true")/*check if password and username are correct*/) {
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 }
